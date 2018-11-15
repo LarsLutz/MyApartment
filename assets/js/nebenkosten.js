6c273389fb8170d2cf381208d8bf33f7bf7074bf
robinwidmer17 <32195633+robinwidmer17@users.noreply.github.com>
2018-11-15 16:01:53 +0100

    do nehm

---
+++ assets/js/nebenkosten.js
@@ -0,0 +1,16 @@
+<script>
+                        function machuf(jh) {
+                           var id = document.getElementById(jh);
+                           if (id.style.display === "none") {
+                               id.style.display = "block";
+                           } else {
+                               id.style.display = "none";
+                            }
+                        }
+                    </script>/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
