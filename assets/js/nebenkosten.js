cab16db48d5b059d9c1b3b40ce8fd1a9f9858903
LarsLutz <32195663+LarsLutz@users.noreply.github.com>
2018-11-29 13:49:00 +0100

    Robin du Noob

--- assets/js/nebenkosten.js
+++ assets/js/nebenkosten.js
@@ -1,4 +1,4 @@
-<script>
+
                         function machuf(jh) {
                            var id = document.getElementById(jh);
                            if (id.style.display === "none") {
@@ -7,10 +7,10 @@
                                id.style.display = "none";
                             }
                         }
-                    </script>/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                    
+// To change this license header, choose License Headers in Project Properties.
+// To change this template file, choose Tools | Templates
+// and open the template in the editor.
+ 
 
 
