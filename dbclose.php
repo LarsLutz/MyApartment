e2a8ef9dfa18d1af192ce8ee06e56b6527aba697
LarsLutz <32195663+LarsLutz@users.noreply.github.com>
2018-12-16 22:52:06 +0100

    Update dbclose.php

--- dbclose.php
+++ dbclose.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(E_ALL);
     error_reporting(E_ALL);
-    $MYSQL_HOST = 'wp333.webpack.hosteurope.de';
-    $MYSQL_USER = 'db12222424-2';
-    $MYSQL_PASS = 'Dominion255#';
-    $MYSQL_DATA = 'db12222424-2';
+    $MYSQL_HOST = '';
+    $MYSQL_USER = '';
+    $MYSQL_PASS = '';
+    $MYSQL_DATA = '';
 
     $connid = mysqli_connect($MYSQL_HOST, $MYSQL_USER,$MYSQL_PASS,$MYSQL_DATA) OR die("Error: ". mysqli_error());
     
